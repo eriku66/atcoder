0a795c2a6510e0242c015e549ad31806915d0f72
eriku66 <eriku.hashimoto@gmail.com>
2024-04-21 22:34:14 +0900

    feat: 🧩 solve ABC350 a

---
+++ abc350/src/bin/a.rs
@@ -0,0 +1,30 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+#[fastout]
+fn main() {
+    input! {
+        s: String,
+    }
+
+    let num = s[3..].parse::<u16>().unwrap();
+
+    if num != 0 && num != 316 && num < 350 {
+        print!("Yes")
+    } else {
+        print!("No")
+    }
+}
