3d1147ac3a250c4c0b40825f49bac8cadfe15300
eriku66 <eriku.hashimoto@gmail.com>
2024-04-21 23:23:44 +0900

    chore: 🛠️ Add git cz config file for this repo

---
+++ changelog.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  disableEmoji: false,
+  format: '{type}{scope}: {emoji}{subject}',
+  list: ['solve', 'fix', 'chore', 'docs', 'refactor', 'perf'],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: ['type', 'scope', 'subject', 'body'],
+  scopes: [],
+  types: {
+    chore: {
+      description: 'Build process or auxiliary tool changes',
+      emoji: '🛠️',
+      value: 'chore'
+    },
+    docs: {
+      description: 'Documentation only changes',
+      emoji: '🖊️',
+      value: 'docs'
+    },
+    solve: {
+      description: 'Solve a problem',
+      emoji: '🤩',
+      value: 'solve'
+    },
+    fix: {
+      description: 'A bug fix',
+      emoji: '👾',
+      value: 'fix'
+    },
+    perf: {
+      description: 'A code change that improves performance',
+      emoji: '⚡️',
+      value: 'perf'
+    },
+    refactor: {
+      description: 'A code change that neither fixes a bug or adds a feature',
+      emoji: '💡',
+      value: 'refactor'
+    },
+    messages: {
+      type: 'Select the type of change that you\'re committing:',
+      customScope: 'Select the scope this component affects:',
+      subject: 'Write a short, imperative mood description of the change:\n',
+      body: 'Provide a longer description of the change:\n ',
+      breaking: 'List any breaking changes:\n',
+      footer: 'Issues this commit closes, e.g #123:',
+      confirmCommit: 'The packages that this commit has affected\n',
+    },
+  }
+};
+
