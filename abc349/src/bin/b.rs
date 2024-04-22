bf62d60ea5e4393a3a2a004a63333b448860ca22
eriku66 <eriku.hashimoto@gmail.com>
2024-04-22 11:11:12 +0900

    solve: 🤩 abc349 b

--- abc349/src/bin/b.rs
+++ abc349/src/bin/b.rs
@@ -16,9 +16,35 @@ use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        s: String,
     }
+
+    let mut char_counts: HashMap<char, usize> = HashMap::new();
+
+    for c in s.chars() {
+        let count = char_counts.entry(c).or_insert(0);
+        *count += 1;
+    }
+
+    let mut char_count_counts: HashMap<usize, usize> = HashMap::new();
+
+    for (_, char_count) in char_counts {
+        let count = char_count_counts.entry(char_count).or_insert(0);
+        *count += 1;
+
+        if *count > 2 {
+            print!("No");
+            return;
+        }
+    }
+
+    for (_, count) in char_count_counts {
+        if count == 1 {
+            print!("No");
+            return;
+        }
+    }
+
+    print!("Yes");
 }
