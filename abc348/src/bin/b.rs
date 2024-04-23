f6c7a3b1ed39aa158f86d814ca6092eba525b198
eriku66 <eriku.hashimoto@gmail.com>
2024-04-24 00:27:42 +0900

    solve: 🤩 abc348 b

--- abc348/src/bin/b.rs
+++ abc348/src/bin/b.rs
@@ -16,9 +16,24 @@ use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
+        points: [(isize, isize); n],
+    }
+
+    for (x1, y1) in &points {
+        let mut farthest_point_index = 0;
+        let mut squared_distance_to_farthest_point = 0;
+
+        for (index, (x2, y2)) in points.iter().enumerate() {
+            let squared_distance = x1.abs_diff(*x2).pow(2) + y1.abs_diff(*y2).pow(2);
+
+            if squared_distance > squared_distance_to_farthest_point {
+                farthest_point_index = index;
+                squared_distance_to_farthest_point = squared_distance;
+            }
+        }
+
+        println!("{}", farthest_point_index + 1);
     }
 }
