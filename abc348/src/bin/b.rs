42116b68d1704d5e6f59b71eb1b081be66ac8c51
eriku66 <eriku.hashimoto@gmail.com>
2024-04-24 09:32:21 +0900

    refactor: 💡 use max_by_key() instead of 'for in'

--- abc348/src/bin/b.rs
+++ abc348/src/bin/b.rs
@@ -22,18 +22,18 @@ fn main() {
     }
 
     for (x1, y1) in &points {
-        let mut farthest_point_index = 0;
-        let mut squared_distance_to_farthest_point = 0;
-
-        for (index, (x2, y2)) in points.iter().enumerate() {
-            let squared_distance = x1.abs_diff(*x2).pow(2) + y1.abs_diff(*y2).pow(2);
-
-            if squared_distance > squared_distance_to_farthest_point {
-                farthest_point_index = index;
-                squared_distance_to_farthest_point = squared_distance;
-            }
-        }
-
-        println!("{}", farthest_point_index + 1);
+        println!(
+            "{}",
+            points
+                .iter()
+                .enumerate()
+                .max_by_key(|&(i, &(x2, y2))| (
+                    x1.abs_diff(x2).pow(2) + y1.abs_diff(y2).pow(2),
+                    -(i as isize)
+                ))
+                .unwrap()
+                .0
+                + 1
+        );
     }
 }
