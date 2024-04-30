00fc3a7f922f31aa0acfd9da12005735ba668fef
eriku66 <eriku.hashimoto@gmail.com>
2024-05-01 01:50:53 +0900

    solve: 🤩 abc350 c

--- abc350/src/bin/c.rs
+++ abc350/src/bin/c.rs
@@ -16,9 +16,26 @@ use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
+        mut a_list: [usize; n],
+    }
+
+    let mut swap_list: Vec<(usize, usize)> = Vec::with_capacity(n);
+
+    for i in 0..n {
+        let mut a = a_list[i];
+
+        while i != a - 1 {
+            a_list.swap(i, a - 1);
+            swap_list.push((i, a - 1));
+            a = a_list[i];
+        }
+    }
+
+    println!("{}", swap_list.len());
+
+    for (i, j) in swap_list {
+        println!("{} {}", i + 1, j + 1);
     }
 }
