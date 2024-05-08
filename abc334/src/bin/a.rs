d030bf306bd903dd008a129c4ffe845aac70c7b1
eriku66 <eriku.hashimoto@gmail.com>
2024-05-09 07:30:25 +0900

    solve: 🤩 abc334 a

---
+++ abc334/src/bin/a.rs
@@ -0,0 +1,33 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+#[fastout]
+fn main() {
+    input! {
+        b: usize,
+        g: usize,
+    }
+
+    print!(
+        "{}",
+        match b.cmp(&g) {
+            Ordering::Greater => "Bat",
+            Ordering::Less => "Glove",
+            _ => "",
+        }
+    )
+}
