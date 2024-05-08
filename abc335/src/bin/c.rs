c716a80da8c5a23a187c54693e235255ca2cd0d2
eriku66 <eriku.hashimoto@gmail.com>
2024-05-08 23:09:51 +0900

    refactor: 💡 Sprit use of input macro

--- abc335/src/bin/c.rs
+++ abc335/src/bin/c.rs
@@ -19,26 +19,31 @@ fn main() {
     input! {
         n: usize,
         q: usize,
-        queries: [(char, String); q],
     }
 
     let mut head_points = (1..=n).rev().map(|i| (i as i32, 0)).collect::<Vec<_>>();
 
-    for (t, c) in queries {
+    for _ in 0..q {
+        input! {t: char};
+
         if t == '1' {
+            input! {c: char}
+
             let mut new_head_point = *head_points.last().unwrap();
 
-            match c.as_str() {
-                "R" => new_head_point.0 += 1,
-                "L" => new_head_point.0 -= 1,
-                "U" => new_head_point.1 += 1,
-                "D" => new_head_point.1 -= 1,
+            match c {
+                'R' => new_head_point.0 += 1,
+                'L' => new_head_point.0 -= 1,
+                'U' => new_head_point.1 += 1,
+                'D' => new_head_point.1 -= 1,
                 _ => {}
             }
 
             head_points.push(new_head_point);
         } else if t == '2' {
-            let point = head_points[head_points.len() - c.parse::<usize>().unwrap()];
+            input! {i: usize};
+
+            let point = head_points[head_points.len() - i];
 
             println!("{} {}", point.0, point.1);
         }
