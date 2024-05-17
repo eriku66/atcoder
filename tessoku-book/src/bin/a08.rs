7bab2a5c6c9481fccdafa9863c53c3be65b28b6b
eriku66 <eriku.hashimoto@gmail.com>
2024-05-17 22:49:42 +0900

    solve: 🤩 tessoku a08

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -17,6 +17,38 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+    }
+
+    let mut grid = vec![vec![0; w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            input! {s: usize}
+
+            grid[i][j] = if j == 0 { s } else { grid[i][j - 1] + s };
+        }
+    }
+
+    input! {
+        q: usize,
+    }
+
+    for _ in 0..q {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        }
+
+        let mut ans = 0;
+
+        for row in grid.iter().skip(a - 1).take(c - a + 1) {
+            ans += row[d - 1] - if b >= 2 { row[b - 2] } else { 0 };
+        }
+
+        println!("{}", ans);
     }
 }
