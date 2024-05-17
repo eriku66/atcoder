9493af8ad110786255b7f1d5339b61b400bf0828
eriku66 <eriku.hashimoto@gmail.com>
2024-05-17 09:05:51 +0900

    solve: 🤩 tessoku b07

--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -17,6 +17,23 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
+        t: usize,
         n: usize,
+        l_r_list: [(usize, usize); n],
+    }
+
+    let mut set = vec![0; t + 1];
+
+    for (l, r) in l_r_list {
+        set[l] += 1;
+        set[r] -= 1;
+    }
+
+    let mut current = 0;
+
+    for c in set.iter().take(t) {
+        current += c;
+
+        println!("{}", current);
     }
 }
