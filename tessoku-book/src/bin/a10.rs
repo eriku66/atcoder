3e044b14262910822a44ab8059a0f46da5df19d3
eriku66 <eriku.hashimoto@gmail.com>
2024-05-19 02:52:42 +0900

    solve: 🤩 tessoku a10

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -18,5 +18,26 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n],
+        d: usize,
+        lr_list: [(usize, usize); d],
+    }
+
+    let max_list = a_list.iter().skip(1).fold(vec![a_list[0]], |mut l, a| {
+        l.push(*l.last().unwrap().max(a));
+        l
+    });
+
+    let r_max_list = a_list
+        .iter()
+        .rev()
+        .skip(1)
+        .fold(vec![*a_list.last().unwrap()], |mut l, a| {
+            l.push(*l.last().unwrap().max(a));
+            l
+        });
+
+    for (l, r) in lr_list {
+        println!("{}", max_list[l - 2].max(r_max_list[n - r - 1]));
     }
 }
