a6555e9ae86260c565d5e23aa46b18f0c7862167
eriku66 <eriku.hashimoto@gmail.com>
2024-05-18 23:23:32 +0900

    solve: 🤩 tessoku b08

--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -18,5 +18,38 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        xy_list: [(usize, usize); n],
+        q: usize,
+        abcd_list: [(usize, usize, usize, usize); q],
+    }
+
+    const LENGTH: usize = 1500;
+
+    let mut grid = vec![vec![0; LENGTH + 1]; LENGTH + 1];
+
+    for (x, y) in xy_list {
+        grid[x][y] += 1;
+    }
+
+    for row in &mut grid {
+        let mut t = 0;
+
+        for c in row {
+            t += *c;
+            *c = t;
+        }
+    }
+
+    for i in 1..=LENGTH {
+        for j in 0..=LENGTH {
+            grid[i][j] += grid[i - 1][j];
+        }
+    }
+
+    for (a, b, c, d) in abcd_list {
+        println!(
+            "{}",
+            grid[c][d] + grid[a - 1][b - 1] - grid[a - 1][d] - grid[c][b - 1]
+        )
     }
 }
