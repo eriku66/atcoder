f8f359e486c2c202dde2f879419cecbc080d14f7
eriku66 <eriku.hashimoto@gmail.com>
2024-05-18 23:03:46 +0900

    solve: 🤩 abc330 c

--- abc330/src/bin/c.rs
+++ abc330/src/bin/c.rs
@@ -14,11 +14,20 @@ use std::cmp::{max, min};
 use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
-use std::ops::Add;
 
 #[fastout]
 fn main() {
     input! {
-        d: isize,
+        d: usize,
     }
+
+    let mut ans = usize::MAX;
+
+    for x in 0..=d.sqrt() {
+        let over = d - x.pow(2);
+        let min_y = over.sqrt();
+        ans = ans.min(min(over - min_y.pow(2), (min_y + 1).pow(2) - over));
+    }
+
+    print!("{}", ans);
 }
