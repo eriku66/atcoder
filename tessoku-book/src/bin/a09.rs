27be3803c9cf62cd569e3752f502c184adef288c
eriku66 <eriku.hashimoto@gmail.com>
2024-05-19 01:03:54 +0900

    solve: 🤩 tessoku a09

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -17,6 +17,33 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
+        h: usize,
+        w: usize,
         n: usize,
+        abcd_list: [(usize, usize, usize, usize); n],
+    }
+
+    let mut grid = vec![vec![0; w + 2]; h + 2];
+
+    for (a, b, c, d) in abcd_list {
+        for row in grid.iter_mut().skip(a).take(c - a + 1) {
+            row[b] += 1;
+            row[d + 1] -= 1;
+        }
+    }
+
+    for row in grid.iter().skip(1).take(h) {
+        let mut t = 0;
+        println!(
+            "{}",
+            row.iter()
+                .skip(1)
+                .take(w)
+                .map(|&c| {
+                    t += c;
+                    t
+                })
+                .join(" ")
+        );
     }
 }
