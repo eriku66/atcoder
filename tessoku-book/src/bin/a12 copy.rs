c9d64038f234fa462e21439028b7061151377d5d
eriku66 <eriku.hashimoto@gmail.com>
2024-05-21 23:28:23 +0900

    study: 🧐 tesssoku a12

--- tessoku-book/src/bin/a12 copy.rs
+++ tessoku-book/src/bin/a12 copy.rs
@@ -22,4 +22,17 @@ fn main() {
         k: usize,
         a_list: [usize; n],
     }
+
+    let mut l = 1;
+    let mut r = 10usize.pow(9);
+
+    while l < r {
+        let m = (l + r) / 2;
+        match k.cmp(&(a_list.iter().map(|a| m / a).sum::<usize>())) {
+            Ordering::Greater => l = m + 1,
+            _ => r = m,
+        }
+    }
+
+    print!("{}", l);
 }
