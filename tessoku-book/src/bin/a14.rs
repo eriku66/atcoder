5843af8bb84023d46823bfca809cb537531edc47
eriku66 <eriku.hashimoto@gmail.com>
2024-05-25 20:59:33 +0900

    solve: 🤩 tessoku a14 with deprecation

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -7,6 +7,7 @@ use proconio::{
     fastout, input,
     marker::{Chars, Usize1},
 };
+use std::cmp::Ordering;
 #[allow(unused_imports)]
 use std::cmp::{max, min};
 #[allow(unused_imports)]
@@ -18,5 +19,48 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        mut a_list: [usize; n],
+        mut b_list: [usize; n],
+        mut c_list: [usize; n],
+        mut d_list: [usize; n],
     }
+
+    a_list.sort_unstable();
+    b_list.sort_unstable();
+    c_list.sort_unstable();
+    d_list.sort_unstable();
+
+    for a in a_list {
+        if a >= k - 3 {
+            break;
+        }
+
+        for &b in &b_list {
+            if a + b >= k - 2 {
+                break;
+            }
+
+            for &c in &c_list {
+                if a + b + c >= k - 1 {
+                    break;
+                }
+
+                for &d in &d_list {
+                    match (a + b + c + d).cmp(&k) {
+                        Ordering::Equal => {
+                            print!("Yes");
+                            return;
+                        }
+                        Ordering::Greater => {
+                            break;
+                        }
+                        Ordering::Less => {}
+                    }
+                }
+            }
+        }
+    }
+
+    print!("No");
 }
