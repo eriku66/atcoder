a41a038b4cb9125452b48d1a201e439ee3d38dc0
eriku66 <eriku.hashimoto@gmail.com>
2024-05-27 08:52:07 +0900

    solve: 🤩 tessoku a14 by half-full enumeration

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -7,9 +7,9 @@ use proconio::{
     fastout, input,
     marker::{Chars, Usize1},
 };
-use std::cmp::Ordering;
 #[allow(unused_imports)]
 use std::cmp::{max, min};
+use std::collections::BTreeSet;
 #[allow(unused_imports)]
 use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
@@ -26,39 +26,26 @@ fn main() {
         mut d_list: [usize; n],
     }
 
-    a_list.sort_unstable();
-    b_list.sort_unstable();
-    c_list.sort_unstable();
-    d_list.sort_unstable();
+    let mut ab_list = BTreeSet::new();
 
     for a in a_list {
-        if a >= k - 3 {
-            break;
-        }
-
         for &b in &b_list {
-            if a + b >= k - 2 {
-                break;
-            }
+            ab_list.insert(a + b);
+        }
+    }
 
-            for &c in &c_list {
-                if a + b + c >= k - 1 {
-                    break;
-                }
+    let mut cd_list = BTreeSet::new();
 
-                for &d in &d_list {
-                    match (a + b + c + d).cmp(&k) {
-                        Ordering::Equal => {
-                            print!("Yes");
-                            return;
-                        }
-                        Ordering::Greater => {
-                            break;
-                        }
-                        Ordering::Less => {}
-                    }
-                }
-            }
+    for c in c_list {
+        for &d in &d_list {
+            cd_list.insert(c + d);
+        }
+    }
+
+    for ab in ab_list {
+        if cd_list.contains(&(k - ab)) {
+            print!("Yes");
+            return;
         }
     }
 
