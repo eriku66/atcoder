a4eb993db8044fa3aafa2a8644dd8eb182fa70d6
eriku66 <eriku.hashimoto@gmail.com>
2024-05-28 01:26:27 +0900

    solve: 🤩 tessoku b14

--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -18,5 +18,45 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        mut a_list: [usize; n],
     }
+
+    let mut first_half = HashSet::new();
+    first_half.insert(0);
+    let mut second_half = HashSet::new();
+    second_half.insert(0);
+
+    for &a in a_list.iter().take(n / 2) {
+        let mut set = HashSet::new();
+
+        for &b in &first_half {
+            set.insert(b + a);
+        }
+
+        set.insert(a);
+
+        first_half.extend(set);
+    }
+
+    for &a in a_list.iter().skip(n / 2) {
+        let mut set = HashSet::new();
+
+        for &b in &second_half {
+            set.insert(b + a);
+        }
+
+        set.insert(a);
+
+        second_half.extend(set);
+    }
+
+    for t in first_half {
+        if second_half.contains(&(k - t)) {
+            print!("Yes");
+            return;
+        }
+    }
+
+    print!("No");
 }
