98c8dd1d7373b695a5a370b6141533f659d91506
eriku66 <eriku.hashimoto@gmail.com>
2024-05-28 21:49:26 +0900

    solve: 🤩 tessoku a16

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -18,5 +18,20 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n - 1],
+        b_list: [usize; n - 2],
     }
+
+    let mut dp = vec![usize::MAX; n];
+    dp[0] = 0;
+
+    for i in 1..n {
+        dp[i] = dp[i].min(dp[i - 1] + a_list[i - 1]);
+
+        if i >= 2 {
+            dp[i] = dp[i].min(dp[i - 2] + b_list[i - 2]);
+        }
+    }
+
+    print!("{}", dp.last().unwrap());
 }
