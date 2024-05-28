afda32c5fd9a9220fbd91a3213fa83bb7c67df21
eriku66 <eriku.hashimoto@gmail.com>
2024-05-28 23:00:53 +0900

    solve: 🤩 tessoku b16

--- tessoku-book/src/bin/b16.rs
+++ tessoku-book/src/bin/b16.rs
@@ -18,5 +18,18 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        h_list: [usize; n],
     }
+
+    let mut dp = vec![0; n];
+    dp[1] = h_list[0].abs_diff(h_list[1]);
+
+    for i in 2..n {
+        dp[i] = min(
+            dp[i - 1] + h_list[i - 1].abs_diff(h_list[i]),
+            dp[i - 2] + h_list[i - 2].abs_diff(h_list[i]),
+        );
+    }
+
+    print!("{}", dp.last().unwrap());
 }
