25bbe883f02d8091f94602ddb73595df35cb9b1e
eriku66 <eriku.hashimoto@gmail.com>
2024-05-28 09:05:36 +0900

    solve: 🤩 tessoku a15

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -18,5 +18,29 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n],
     }
+
+    let mut sorted_a_list = a_list
+        .iter()
+        .enumerate()
+        .map(|(i, &a)| (a, i))
+        .collect_vec();
+
+    sorted_a_list.sort_unstable();
+
+    let mut i = 0;
+    let mut front = 0;
+    let mut ans = vec![0; n];
+
+    for (a, index) in sorted_a_list {
+        if a != front {
+            i += 1;
+        }
+
+        front = a;
+        ans[index] = i;
+    }
+
+    print!("{}", ans.iter().join(" "));
 }
