e0f5adc614d095c6c5be8ac7c6a256afbd09dd2c
eriku66 <eriku.hashimoto@gmail.com>
2024-06-01 03:45:13 +0900

    solve: 🤩 tessoku b17

--- tessoku-book/src/bin/b17.rs
+++ tessoku-book/src/bin/b17.rs
@@ -1,4 +1,11 @@
 #[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+#[allow(unused_imports)]
 use itertools::{iproduct, Itertools};
 #[allow(unused_imports)]
 use num_traits::pow;
@@ -7,16 +14,39 @@ use proconio::{
     fastout, input,
     marker::{Chars, Usize1},
 };
-#[allow(unused_imports)]
-use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{HashMap, HashSet, VecDeque};
-#[allow(unused_imports)]
-use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
+        h_list: [usize; n],
     }
+
+    let mut dp = vec![0; n + 1];
+    dp[2] = h_list[0].abs_diff(h_list[1]);
+
+    for i in 3..=n {
+        dp[i] = min(
+            dp[i - 1] + h_list[i - 2].abs_diff(h_list[i - 1]),
+            dp[i - 2] + h_list[i - 3].abs_diff(h_list[i - 1]),
+        );
+    }
+
+    let mut ans = Vec::with_capacity(n);
+    let mut i = n;
+
+    loop {
+        ans.push(i);
+
+        if i == 1 { break; }
+
+        if dp[i] - dp[i - 1] == h_list[i - 1].abs_diff(h_list[i - 2]) {
+            i -= 1;
+        } else {
+            i -= 2;
+        }
+    }
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter().rev().join(" "));
 }
