b23c871f28e66b9d2e30c2b0194f8858cb243278
eriku66 <eriku.hashimoto@gmail.com>
2024-06-03 23:24:31 +0900

    study: 🧐 tessoku a18

--- tessoku-book/src/bin/a18 copy.rs
+++ tessoku-book/src/bin/a18 copy.rs
@@ -22,4 +22,27 @@ fn main() {
         s: usize,
         a_list: [usize; n],
     }
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for (i, &a) in a_list.iter().enumerate() {
+        let mut new_dp = vec![false; s + 1];
+
+        for (j, _) in dp[i].iter().enumerate().filter(|(_, &c)| c) {
+            new_dp[j] = true;
+
+            if j + a <= s {
+                new_dp[j + a] = true;
+            }
+        }
+
+        dp[i + 1] = new_dp;
+    }
+
+    if dp[n][s] {
+        print!("Yes");
+    } else {
+        print!("No");
+    }
 }
