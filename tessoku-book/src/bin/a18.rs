201acc65cf0c3015f7b573d0509d23d4ff54ea0c
eriku66 <eriku.hashimoto@gmail.com>
2024-06-04 21:17:58 +0900

    solve: 🤩 tessoku a18

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -22,4 +22,27 @@ fn main() {
         s: usize,
         a_list: [usize; n],
     }
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for (i, &a) in a_list.iter().enumerate() {
+        let mut new_row = vec![false; s + 1];
+
+        for (j, _) in dp[i].iter().enumerate().filter(|(_, &c)| c) {
+            new_row[j] = true;
+
+            if s >= j + a {
+                new_row[j + a] = true;
+            }
+        }
+
+        dp[i + 1] = new_row;
+    }
+
+    if dp[n][s] {
+        print!("Yes");
+    } else {
+        print!("No");
+    }
 }
