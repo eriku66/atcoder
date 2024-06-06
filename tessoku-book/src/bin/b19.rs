58882c97600f18b87bb32c576d3bb5b3ec1c5111
eriku66 <eriku.hashimoto@gmail.com>
2024-06-06 22:31:28 +0900

    solve: 🤩 tessoku b19

--- tessoku-book/src/bin/b19.rs
+++ tessoku-book/src/bin/b19.rs
@@ -18,5 +18,32 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        max_weight: isize,
+        wv_list: [(isize, isize); n],
     }
+
+    const V_MAX: usize = 100000;
+
+    let mut dp = vec![vec![-1; V_MAX + 1]; n + 1];
+    dp[0][0] = 0;
+
+    for (i, &(w, v)) in wv_list.iter().enumerate() {
+        for j in 0..=V_MAX {
+            if dp[i][j] == -1 {
+                continue;
+            }
+
+            if dp[i + 1][j] == -1 {
+                dp[i + 1][j] = dp[i][j];
+            } else {
+                dp[i + 1][j] = dp[i + 1][j].min(dp[i][j]);
+            }
+
+            if max_weight >= dp[i][j] + w {
+                dp[i + 1][j + v as usize] = dp[i][j] + w;
+            }
+        }
+    }
+
+    print!("{:?}", dp[n].iter().rposition(|&c| c != -1).unwrap());
 }
