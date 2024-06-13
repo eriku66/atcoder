131f7ee9f9ee3d6aa210b10b1f7ec752aa7b1629
eriku66 <eriku.hashimoto@gmail.com>
2024-06-13 22:17:31 +0900

    solve: 🤩 tessoku a29

--- tessoku-book/src/bin/a29.rs
+++ tessoku-book/src/bin/a29.rs
@@ -17,6 +17,34 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        a: u64,
+        b: u32,
     }
+
+    const D: u64 = 1000000007;
+
+    let mut squares: Vec<u64> = vec![0; (b.ilog2() + 1) as usize];
+    squares[0] = a;
+
+    for i in 1..squares.len() {
+        squares[i] = squares[i - 1] * squares[i - 1];
+
+        if squares[i] > D {
+            squares[i] %= D;
+        }
+    }
+
+    let mut ans = 1;
+
+    for (i, &s) in squares.iter().enumerate() {
+        if b / 2u32.pow(i as u32) % 2 == 1 {
+            ans *= s;
+
+            if ans > D {
+                ans %= D;
+            }
+        }
+    }
+
+    print!("{}", ans % D);
 }
