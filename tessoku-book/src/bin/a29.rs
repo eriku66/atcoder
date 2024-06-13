b0598f2d8c41667894bda4eec09ea1935d49e668
eriku66 <eriku.hashimoto@gmail.com>
2024-06-13 22:43:40 +0900

    study: 🧐 tessoku a29

--- tessoku-book/src/bin/a29.rs
+++ tessoku-book/src/bin/a29.rs
@@ -14,6 +14,22 @@ use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
 
+fn power(a: u64, b: u32, m: u64) -> u64 {
+    let mut p = a;
+    let mut ans = 1;
+
+    for i in 0..30 {
+        if b / (1 << i) % 2 == 1 {
+            ans *= p;
+            ans %= m;
+        }
+
+        p = (p * p) % m;
+    }
+
+    ans
+}
+
 #[fastout]
 fn main() {
     input! {
@@ -21,30 +37,5 @@ fn main() {
         b: u32,
     }
 
-    const D: u64 = 1000000007;
-
-    let mut squares: Vec<u64> = vec![0; (b.ilog2() + 1) as usize];
-    squares[0] = a;
-
-    for i in 1..squares.len() {
-        squares[i] = squares[i - 1] * squares[i - 1];
-
-        if squares[i] > D {
-            squares[i] %= D;
-        }
-    }
-
-    let mut ans = 1;
-
-    for (i, &s) in squares.iter().enumerate() {
-        if b / 2u32.pow(i as u32) % 2 == 1 {
-            ans *= s;
-
-            if ans > D {
-                ans %= D;
-            }
-        }
-    }
-
-    print!("{}", ans % D);
+    print!("{}", power(a, b, 1000000007));
 }
