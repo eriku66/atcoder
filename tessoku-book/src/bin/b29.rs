60ea50c7155ad22a8f05d3c96842063f12c03b01
eriku66 <eriku.hashimoto@gmail.com>
2024-06-14 22:17:01 +0900

    solve: 🤩 tessoku b29

--- tessoku-book/src/bin/b29.rs
+++ tessoku-book/src/bin/b29.rs
@@ -17,6 +17,24 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        a: u64,
+        b: u64,
     }
+
+    const M: u64 = 1000000007;
+
+    let mut p = a;
+    let mut ans = 1;
+
+    for i in 0..=b.ilog2() {
+        if b / (1 << i) % 2 == 1 {
+            ans *= p;
+            ans %= M;
+        }
+
+        p *= p;
+        p %= M;
+    }
+
+    print!("{}", ans);
 }
