c69da58bbfbf0db239df6ca97a6decbde195fd76
eriku66 <eriku.hashimoto@gmail.com>
2024-06-18 00:43:01 +0900

    study: 🧐 tessoku b30 using ac_library and fold()

--- tessoku-book/src/bin/b30 copy 1.rs
+++ tessoku-book/src/bin/b30 copy 1.rs
@@ -15,10 +15,19 @@ use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
 
+fn fractorial_mod(n: usize) -> Mint {
+    (1..=n).fold(Mint::new(1), |acc, i| acc * i)
+}
+
 #[fastout]
 fn main() {
     input! {
         h: usize,
         w: usize,
     }
+
+    let num = fractorial_mod(h + w - 2);
+    let den = fractorial_mod(h - 1) * fractorial_mod(w - 1);
+
+    print!("{}", num * den.inv());
 }
