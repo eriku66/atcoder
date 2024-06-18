921095b1f938acbbf48881d7725359605f655521
eriku66 <eriku.hashimoto@gmail.com>
2024-06-19 01:06:30 +0900

    solve: 🤩 tessoku a20

--- tessoku-book/src/bin/a20.rs
+++ tessoku-book/src/bin/a20.rs
@@ -20,4 +20,18 @@ fn main() {
         s: Chars,
         t: Chars,
     }
+
+    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+
+    for (i, sc) in s.iter().enumerate() {
+        for (j, tc) in t.iter().enumerate() {
+            dp[i + 1][j + 1] = if sc == tc {
+                (dp[i][j] + 1).max(dp[i + 1][j]).max(dp[i][j + 1])
+            } else {
+                dp[i + 1][j].max(dp[i][j + 1])
+            }
+        }
+    }
+
+    print!("{}", dp[s.len()][t.len()]);
 }
