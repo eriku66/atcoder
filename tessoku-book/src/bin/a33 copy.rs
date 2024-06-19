42ad1fae07aa68d4d0eb31db20a5f25674c491e6
eriku66 <eriku.hashimoto@gmail.com>
2024-06-19 22:13:18 +0900

    study: 🧐 tessoku a33

--- tessoku-book/src/bin/a33 copy.rs
+++ tessoku-book/src/bin/a33 copy.rs
@@ -20,4 +20,23 @@ fn main() {
         n: usize,
         a_list: [usize; n],
     }
+
+    // 山が2つの時は物真似戦略が使える
+    // 相手と同じ数を取ることで必勝
+    // if n == 2 {
+    //     if a_list[0] == a_list[1] {
+    //         print!("Second");
+    //     } else {
+    //         print!("First");
+    //     }
+
+    //     return;
+    // }
+
+    // ニム和だと3つ以上の山でもいける
+    if a_list.iter().fold(0, |acc, &a| acc ^ a) == 0 {
+        print!("Second");
+    } else {
+        print!("First");
+    }
 }
