101517d9786410035c2070b876d24bc0cfc6d8a2
eriku66 <eriku.hashimoto@gmail.com>
2024-06-20 23:04:35 +0900

    study: 🧐 tessoku a34

--- tessoku-book/src/bin/a34 copy.rs
+++ tessoku-book/src/bin/a34 copy.rs
@@ -22,4 +22,24 @@ fn main() {
         y: usize,
         a_list: [usize; n],
     }
+
+    let mut grundy = vec![0; 100_001];
+
+    for i in 0..grundy.len() {
+        grundy[i] = (0..=2)
+            .filter(|&g| !(i >= x && g == grundy[i - x] || i >= y && g == grundy[i - y]))
+            .min()
+            .unwrap();
+    }
+
+    if a_list
+        .iter()
+        .map(|&a| grundy[a])
+        .fold(0, |acc, ga| acc ^ ga)
+        == 0
+    {
+        print!("Second");
+    } else {
+        print!("First");
+    }
 }
