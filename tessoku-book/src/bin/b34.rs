52c4bee7aa5c8390c574de151b1c18dbd9713425
eriku66 <eriku.hashimoto@gmail.com>
2024-06-20 23:20:26 +0900

    solve: 🤩 tessoku b34

--- tessoku-book/src/bin/b34.rs
+++ tessoku-book/src/bin/b34.rs
@@ -18,5 +18,28 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        _x: usize,
+        _y: usize,
+        a_list: [usize; n],
+    }
+
+    fn find_grundy_number(a: usize) -> usize {
+        match a % 5 {
+            0 | 1 => 0,
+            2 | 3 => 1,
+            4 => 2,
+            _ => 0,
+        }
+    }
+
+    if a_list
+        .iter()
+        .map(|&a| find_grundy_number(a))
+        .fold(0, |acc, ga| acc ^ ga)
+        == 0
+    {
+        print!("Second");
+    } else {
+        print!("First");
     }
 }
