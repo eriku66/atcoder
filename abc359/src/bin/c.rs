9018d49b89ec300052fd440521d491ccc6af9e06
eriku66 <eriku.hashimoto@gmail.com>
2024-06-23 14:00:43 +0900

    refactor: 💡 simplify
    \

--- abc359/src/bin/c.rs
+++ abc359/src/bin/c.rs
@@ -17,33 +17,30 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        sx: usize,
+        mut sx: usize,
         sy: usize,
-        tx: usize,
+        mut tx: usize,
         ty: usize,
     }
 
-    let mut ans = 0;
+    if sx.abs_diff(sy) % 2 == 1 {
+        sx -= 1;
+    }
+
+    if tx.abs_diff(ty) % 2 == 1 {
+        tx -= 1;
+    }
 
     let mut x_diff = sx.abs_diff(tx);
 
-    if x_diff > 0 && sy % 2 == 1 && (sx % 2 == 0 && sx < tx || sx % 2 == 1 && sx > tx) {
-        x_diff += 1;
-    }
-
-    if x_diff > 0 && sy % 2 == 0 && (sx % 2 == 1 && sx < tx || sx % 2 == 0 && sx > tx) {
-        x_diff += 1;
-    }
-
     let y_diff = sy.abs_diff(ty);
 
+    let mut ans = y_diff;
+
     if x_diff >= y_diff {
-        ans += y_diff;
         x_diff -= y_diff;
 
         ans += x_diff / 2;
-    } else {
-        ans += y_diff;
     }
 
     print!("{}", ans);
