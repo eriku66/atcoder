4d02b78b730deaa5a6a3f354a8303cf30fa8f1f2
eriku66 <eriku.hashimoto@gmail.com>
2024-06-23 20:56:49 +0900

    solve: 🤩 tessoku a21

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -18,5 +18,47 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        pa_list: [(Usize1, usize); n],
     }
+
+    let mut dp = vec![vec![0; n]; n];
+
+    // dp[0][r]
+    for r in (0..n - 1).rev() {
+        dp[0][r] = dp[0][r + 1];
+
+        if pa_list[r + 1].0 <= r {
+            dp[0][r] += pa_list[r + 1].1;
+        }
+    }
+
+    // dp[l][n - 1]
+    for l in 1..n {
+        dp[l][n - 1] = dp[l - 1][n - 1];
+
+        if l <= pa_list[l - 1].0 {
+            dp[l][n - 1] += pa_list[l - 1].1;
+        }
+    }
+
+    for l in 1..n {
+        for r in (l..n - 1).rev() {
+            dp[l][r] = max(
+                dp[l][r + 1]
+                    + if l <= pa_list[r + 1].0 && pa_list[r + 1].0 <= r {
+                        pa_list[r + 1].1
+                    } else {
+                        0
+                    },
+                dp[l - 1][r]
+                    + if l <= pa_list[l - 1].0 && pa_list[l - 1].0 <= r {
+                        pa_list[l - 1].1
+                    } else {
+                        0
+                    },
+            );
+        }
+    }
+
+    print!("{}", (0..n).map(|i| dp[i][i]).max().unwrap());
 }
