bc31c82df6add3da2437308a6d7d679dca26dc62
eriku66 <eriku.hashimoto@gmail.com>
2024-06-30 11:11:07 +0900

    solve: 🤩 tessoku a40

--- tessoku-book/src/bin/a40.rs
+++ tessoku-book/src/bin/a40.rs
@@ -18,5 +18,20 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n],
     }
+
+    let mut l = HashMap::with_capacity(n);
+
+    for a in a_list {
+        *l.entry(a).or_insert(0) += 1;
+    }
+
+    print!(
+        "{}",
+        l.values()
+            .filter(|&&c| c >= 3)
+            .map(|&c| c * (c - 1) * (c - 2) / 6)
+            .sum::<usize>()
+    );
 }
