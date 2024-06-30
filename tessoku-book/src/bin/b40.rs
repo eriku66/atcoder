541d563a7e0e452c580339eb03035293531fa465
eriku66 <eriku.hashimoto@gmail.com>
2024-06-30 13:12:20 +0900

    solve: 🤩 tessoku b40

--- tessoku-book/src/bin/b40.rs
+++ tessoku-book/src/bin/b40.rs
@@ -18,5 +18,28 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n],
     }
+
+    let mut l = vec![0; 100];
+
+    for a in a_list {
+        l[a % 100] += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 1..50 {
+        ans += l[i] * l[100 - i];
+    }
+
+    if l[0] >= 2 {
+        ans += l[0] * (l[0] - 1) / 2;
+    };
+
+    if l[50] >= 2 {
+        ans += l[50] * (l[50] - 1) / 2;
+    };
+
+    print!("{}", ans);
 }
