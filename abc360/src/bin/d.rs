e14a820fc10523027c5da0a61d2456db62ad5447
eriku66 <eriku.hashimoto@gmail.com>
2024-07-01 08:10:59 +0900

    solve: 🤩 abc360 d

--- abc360/src/bin/d.rs
+++ abc360/src/bin/d.rs
@@ -22,4 +22,35 @@ fn main() {
         s: Chars,
         x_list: [isize; n],
     }
+
+    let mut p_list = Vec::new();
+    let mut m_list = Vec::new();
+
+    for (i, &c) in s.iter().enumerate() {
+        match c {
+            '0' => m_list.push(x_list[i]),
+            '1' => p_list.push(x_list[i]),
+            _ => {}
+        }
+    }
+
+    if p_list.is_empty() || m_list.is_empty() {
+        print!("0");
+        return;
+    }
+
+    p_list.sort_unstable();
+    m_list.sort_unstable();
+
+    let mut ans = 0;
+
+    for p in p_list {
+        let l = m_list.binary_search(&p).map_or_else(|e| e, |v| v + 1);
+        let r = m_list
+            .binary_search(&(t * 2 + p))
+            .map_or_else(|e| e, |v| v + 1);
+        ans += r - l;
+    }
+
+    print!("{}", ans);
 }
