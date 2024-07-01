ccd5be6e164bf6285a1ecd0ee0a29615f00f7dba
eriku66 <eriku.hashimoto@gmail.com>
2024-07-01 23:02:24 +0900

    study: 🧐 tessoku a42

--- tessoku-book/src/bin/a42 copy.rs
+++ tessoku-book/src/bin/a42 copy.rs
@@ -21,4 +21,19 @@ fn main() {
         k: usize,
         ab_list: [(usize, usize); n],
     }
+
+    let mut max_count = 0;
+
+    for a in 1..=100 {
+        for b in 1..=100 {
+            max_count = max_count.max(
+                ab_list
+                    .iter()
+                    .filter(|&&(aa, bb)| a <= aa && aa <= a + k && b <= bb && bb <= b + k)
+                    .count(),
+            )
+        }
+    }
+
+    print!("{}", max_count);
 }
