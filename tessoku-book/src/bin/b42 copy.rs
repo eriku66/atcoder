a405b9af527ec43591377a51b38371b8f093f018
eriku66 <eriku.hashimoto@gmail.com>
2024-07-02 23:54:24 +0900

    study: 🧐 tessoku b42

--- tessoku-book/src/bin/b42 copy.rs
+++ tessoku-book/src/bin/b42 copy.rs
@@ -20,4 +20,18 @@ fn main() {
         n: usize,
         ab_list: [(i64, i64); n],
     }
+
+    let mut apbp = 0;
+    let mut apbm = 0;
+    let mut ambp = 0;
+    let mut ambm = 0;
+
+    for (a, b) in ab_list {
+        apbp += (a + b).max(0);
+        apbm += (a - b).max(0);
+        ambp += (-a + b).max(0);
+        ambm += (-a - b).max(0);
+    }
+
+    print!("{}", apbp.max(apbm).max(ambp).max(ambm));
 }
