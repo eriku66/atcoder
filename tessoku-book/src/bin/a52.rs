2389d98d38e2420791be84b7d6be8de197c0edc9
eriku66 <eriku.hashimoto@gmail.com>
2024-07-09 23:22:30 +0900

    solve: 🤩 tessoku a52

--- tessoku-book/src/bin/a52.rs
+++ tessoku-book/src/bin/a52.rs
@@ -17,6 +17,26 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        q: usize,
+    }
+
+    let mut queue = VecDeque::new();
+
+    for _ in 0..q {
+        input! {t: usize}
+
+        match t {
+            1 => {
+                input! {x: String}
+                queue.push_back(x);
+            }
+            2 => {
+                println!("{}", queue.front().unwrap());
+            }
+            3 => {
+                queue.pop_front();
+            }
+            _ => {}
+        }
     }
 }
