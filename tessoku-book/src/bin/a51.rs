b91b479f54b2f4623eac860bb1ee0800835a56a7
eriku66 <eriku.hashimoto@gmail.com>
2024-07-09 22:57:31 +0900

    solve: 🤩 tessoku a51

--- tessoku-book/src/bin/a51.rs
+++ tessoku-book/src/bin/a51.rs
@@ -17,6 +17,26 @@ use std::iter::FromIterator;
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        q: usize,
+    }
+
+    let mut stack = Vec::new();
+
+    for _ in 0..q {
+        input! {t: usize}
+
+        match t {
+            1 => {
+                input! {x: String}
+                stack.push(x);
+            }
+            2 => {
+                println!("{}", stack.last().unwrap());
+            }
+            3 => {
+                stack.pop();
+            }
+            _ => {}
+        }
     }
 }
