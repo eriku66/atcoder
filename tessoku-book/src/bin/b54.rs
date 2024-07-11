74b1eec9f9dbc6bf5cab51812678a267ba407943
eriku66 <eriku.hashimoto@gmail.com>
2024-07-11 23:38:19 +0900

    solve: 🤩 tessoku b54

--- tessoku-book/src/bin/b54.rs
+++ tessoku-book/src/bin/b54.rs
@@ -18,5 +18,19 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        a_list: [usize; n],
     }
+
+    let mut map = HashMap::with_capacity(n);
+
+    for a in a_list {
+        *map.entry(a).or_insert(0) += 1;
+    }
+
+    print!(
+        "{}",
+        map.iter()
+            .map(|(_, &c)| if c > 1 { c * (c - 1) / 2 } else { 0 })
+            .sum::<usize>()
+    );
 }
