bf9fe7955af12710b29621894ffcf37fe0dbec07
eriku66 <eriku.hashimoto@gmail.com>
2024-07-18 23:52:28 +0900

    solve: 🤩 tessoku b62

--- tessoku-book/src/bin/b62.rs
+++ tessoku-book/src/bin/b62.rs
@@ -18,5 +18,58 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        mut ab_list: [(usize, usize); m],
+    }
+
+    ab_list.sort_unstable();
+
+    let mut graph = vec![vec![]; n + 1];
+
+    for (a, b) in ab_list {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut visited = Vec::with_capacity(n);
+
+    dfs(
+        1,
+        n,
+        &graph,
+        &mut visited,
+        &mut vec![false; n + 1],
+        &mut false,
+    );
+
+    print!("{}", visited.iter().join(" "));
+}
+
+fn dfs(
+    pos: usize,
+    n: usize,
+    graph: &[Vec<usize>],
+    visited: &mut Vec<usize>,
+    is_visited: &mut [bool],
+    reach: &mut bool,
+) {
+    visited.push(pos);
+    is_visited[pos] = true;
+
+    if pos == n {
+        *reach = true;
+        return;
+    }
+
+    for &next in graph[pos].iter() {
+        if !is_visited[next] {
+            dfs(next, n, graph, visited, is_visited, reach);
+
+            if *reach {
+                break;
+            }
+
+            visited.pop();
+        }
     }
 }
