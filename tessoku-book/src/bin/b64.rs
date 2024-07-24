ff925969fd621f02befda9cb162681adc5413952
eriku66 <eriku.hashimoto@gmail.com>
2024-07-24 23:41:10 +0900

    solve: 🤩 tessoku b64

--- tessoku-book/src/bin/b64.rs
+++ tessoku-book/src/bin/b64.rs
@@ -13,10 +13,48 @@ use std::cmp::{max, min};
 use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        abc_list: [(Usize1, Usize1, isize); m],
     }
+
+    let mut graph = vec![Vec::with_capacity(n); n];
+
+    for (a, b, c) in abc_list {
+        graph[a].push((b, c));
+        graph[b].push((a, c));
+    }
+
+    let mut dists = vec![-1; n];
+    let mut heap = BinaryHeap::new();
+
+    heap.push((Reverse(0), 0));
+
+    while let Some((Reverse(min_d), min_p)) = heap.pop() {
+        if dists[min_p] != -1 {
+            continue;
+        }
+
+        dists[min_p] = min_d;
+
+        for &(p, d) in graph[min_p].iter().filter(|&&(p, _)| dists[p] == -1) {
+            heap.push((Reverse(min_d + d), p))
+        }
+    }
+
+    let mut ans = vec![n - 1];
+
+    while let Some(&(p, _)) = graph[*ans.last().unwrap()]
+        .iter()
+        .find(|&&(p, d)| dists[*ans.last().unwrap()] - d == dists[p])
+    {
+        ans.push(p);
+    }
+
+    print!("{}", ans.iter().rev().map(|&p| p + 1).join(" "));
 }
