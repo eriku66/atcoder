bdfaf29fdb083544b4bab45672f515c5a833b4fc
eriku66 <eriku.hashimoto@gmail.com>
2024-07-29 00:14:28 +0900

    solve: 🤩 abc349 c

--- abc349/src/bin/c.rs
+++ abc349/src/bin/c.rs
@@ -14,11 +14,32 @@ use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
 
-#[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        s: Chars,
+        mut t: String,
     }
+
+    let mut lower_t = t.to_lowercase().chars().collect_vec();
+
+    if lower_t[2] == 'x' {
+        lower_t.pop();
+    }
+
+    let len = lower_t.len();
+
+    let mut i = 0;
+
+    for c in s {
+        if c == lower_t[i] {
+            i += 1;
+
+            if i == len {
+                print!("Yes");
+                return;
+            }
+        }
+    }
+
+    print!("No");
 }
