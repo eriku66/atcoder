062fe72d0c75c6892220ab6580377bcda01a7cf3
eriku66 <eriku.hashimoto@gmail.com>
2024-07-31 07:52:10 +0900

    solve: 🤩 abc348 c

--- abc348/src/bin/c.rs
+++ abc348/src/bin/c.rs
@@ -16,9 +16,17 @@ use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
+        ac_list: [(usize, usize); n],
     }
+
+    let mut l = HashMap::with_capacity(n);
+
+    for (a, c) in ac_list {
+        let v = l.entry(c).or_insert(1_000_000_000);
+        *v = min(*v, a);
+    }
+
+    print!("{}", l.values().max().unwrap());
 }
