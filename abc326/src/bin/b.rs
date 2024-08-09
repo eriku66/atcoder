97e815adbb06e612a935cf36148a97019d3675a2
eriku66 <eriku.hashimoto@gmail.com>
2024-08-09 23:27:32 +0900

    solve: 🤩 abc326 ab

---
+++ abc326/src/bin/b.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+    }
+
+    for m in n..=919 {
+        if m / 100 * (m % 100 / 10) == m % 10 {
+            print!("{}", m);
+            return;
+        }
+    }
+}
