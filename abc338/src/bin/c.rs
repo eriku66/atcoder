a83e3852d9f85e5915f189b1147f1349efd4b2e6
eriku66 <eriku.hashimoto@gmail.com>
2024-08-27 08:54:57 +0900

    solve: 🤩 abc338 c

--- abc338/src/bin/c.rs
+++ abc338/src/bin/c.rs
@@ -18,5 +18,33 @@ use std::iter::FromIterator;
 fn main() {
     input! {
         n: usize,
+        q_list: [usize; n],
+        a_list: [usize; n],
+        b_list: [usize; n],
     }
+
+    let mut ans = 0;
+
+    for a_cnt in 0..=*q_list.iter().max().unwrap() {
+        let b_cnt = q_list
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &q)| {
+                if q < a_list[i] * a_cnt {
+                    Some(0)
+                } else if b_list[i] > 0 {
+                    Some((q - a_list[i] * a_cnt) / b_list[i])
+                } else {
+                    None
+                }
+            })
+            .min()
+            .unwrap_or(0);
+
+        if b_cnt > 0 {
+            ans = max(ans, a_cnt + b_cnt)
+        }
+    }
+
+    print!("{}", ans);
 }
