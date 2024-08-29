82c053326966a1d4b577d0801aac881d6cbf530f
eriku66 <eriku.hashimoto@gmail.com>
2024-08-30 00:33:58 +0900

    solve: 🤩 abc366 d

--- abc366/src/bin/d.rs
+++ abc366/src/bin/d.rs
@@ -20,5 +20,48 @@ use superslice::Ext;
 fn main() {
     input! {
         n: usize,
+        mut n_square: [[[usize; n]; n]; n],
+        q: usize,
+        queries: [(Usize1, Usize1, Usize1, Usize1, Usize1, Usize1); q],
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                if k > 0 {
+                    n_square[i][j][k] += n_square[i][j][k - 1];
+                }
+            }
+
+            for k in 0..n {
+                if i > 0 {
+                    n_square[i][j][k] += n_square[i - 1][j][k];
+                }
+            }
+        }
+    }
+
+    for (lx, rx, ly, ry, lz, rz) in queries {
+        let mut sum = 0;
+
+        for n_row in n_square[rx].iter().skip(ly).take(ry - ly + 1) {
+            sum += n_row[rz];
+
+            if lz > 0 {
+                sum -= n_row[lz - 1];
+            }
+        }
+
+        if lx > 0 {
+            for n_row in n_square[lx - 1].iter().skip(ly).take(ry - ly + 1) {
+                sum -= n_row[rz];
+
+                if lz > 0 {
+                    sum += n_row[lz - 1];
+                }
+            }
+        }
+
+        println!("{}", sum);
     }
 }
