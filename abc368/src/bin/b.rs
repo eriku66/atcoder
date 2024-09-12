8183be0a884ece5a5925461b88d81e1b2b37da1c
eriku66 <eriku.hashimoto@gmail.com>
2024-09-12 23:28:18 +0900

    solve: 🤩 abc368 b

--- abc368/src/bin/b.rs
+++ abc368/src/bin/b.rs
@@ -16,9 +16,37 @@ use std::iter::FromIterator;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
+// #[fastout]
 fn main() {
     input! {
         n: usize,
+        mut a_list: [usize; n],
     }
+
+    let mut zero_count = a_list.iter().filter(|&&a| a == 0).count();
+    let mut ans = 0;
+
+    while zero_count < n - 1 {
+        a_list.sort_unstable_by(|a, b| b.cmp(a));
+
+        if a_list[0] > 0 {
+            a_list[0] -= 1;
+
+            if a_list[0] == 0 {
+                zero_count += 1;
+            }
+        }
+
+        if a_list[1] > 0 {
+            a_list[1] -= 1;
+
+            if a_list[1] == 0 {
+                zero_count += 1;
+            }
+        }
+
+        ans += 1;
+    }
+
+    print!("{}", ans);
 }
