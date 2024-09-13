5b7acf4f220d1efbd67e928e851fbeffa085e211
eriku66 <eriku.hashimoto@gmail.com>
2024-09-13 23:45:02 +0900

    solve: 🤩 abc368 c

--- abc368/src/bin/c.rs
+++ abc368/src/bin/c.rs
@@ -16,9 +16,31 @@ use std::iter::FromIterator;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
+// #[fastout]
 fn main() {
     input! {
         n: usize,
+        mut h_list: [isize; n],
     }
+
+    let mut t = 0;
+    let mut i = 0;
+
+    while i < n {
+        let d = h_list[i] / 5;
+
+        if d > 0 {
+            t += d * 3;
+            h_list[i] -= d * 5;
+        } else {
+            t += 1;
+            h_list[i] -= if t % 3 == 0 { 3 } else { 1 };
+        }
+
+        if h_list[i] <= 0 {
+            i += 1;
+        }
+    }
+
+    print!("{}", t);
 }
