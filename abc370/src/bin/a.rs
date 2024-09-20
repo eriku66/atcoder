c552301daeffabfba260596b96fc4c12fb8748e6
eriku66 <eriku.hashimoto@gmail.com>
2024-09-20 23:54:17 +0900

    solve: 🤩 abc370 a

---
+++ abc370/src/bin/a.rs
@@ -0,0 +1,35 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input! {
+        l: usize,
+        r: usize,
+    }
+
+    print!(
+        "{}",
+        if l == r {
+            "Invalid"
+        } else if l == 1 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
+}
