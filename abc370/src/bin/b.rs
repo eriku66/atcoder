eb3de66f3aaf639e2faee9a75c2c5b6a112af0d5
eriku66 <eriku.hashimoto@gmail.com>
2024-09-21 23:05:34 +0900

    solve: 🤩 abc370 b

--- abc370/src/bin/b.rs
+++ abc370/src/bin/b.rs
@@ -16,9 +16,28 @@ use std::iter::FromIterator;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
 fn main() {
     input! {
         n: usize,
     }
+
+    let mut a_list = Vec::new();
+
+    for i in 1..=n {
+        input! {a: [Usize1; i]}
+
+        a_list.push(a);
+    }
+
+    let mut j = 0;
+
+    for i in 0..n {
+        if i >= j {
+            j = a_list[i][j];
+        } else {
+            j = a_list[j][i];
+        }
+    }
+
+    print!("{}", j + 1);
 }
