148d9926349fe5ee00e07271cdc3507f681b0e94
eriku66 <eriku.hashimoto@gmail.com>
2024-10-04 23:04:21 +0900

    solve: 🤩 abc373 a

---
+++ abc373/src/bin/a.rs
@@ -0,0 +1,32 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input! {
+        s_list: [String; 12],
+    }
+
+    print!(
+        "{}",
+        s_list
+            .iter()
+            .enumerate()
+            .filter(|&(i, s)| i + 1 == s.len())
+            .count()
+    );
+}
