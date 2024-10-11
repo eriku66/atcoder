746590252c48762c967db9447c25a0f34a8e9d96
eriku66 <eriku.hashimoto@gmail.com>
2024-10-11 23:44:39 +0900

    solve: 🤩 abc374 c

--- abc374/src/bin/c.rs
+++ abc374/src/bin/c.rs
@@ -19,5 +19,28 @@ use superslice::Ext;
 fn main() {
     input! {
         n: usize,
+        k_list: [usize; n],
     }
+
+    let k_total = k_list.iter().sum::<usize>();
+    let k_total_half = (k_total + 1) / 2;
+
+    let mut ans = k_total;
+
+    for i in (1..=n).rev() {
+        for combination in k_list.iter().combinations(i) {
+            let total = combination.into_iter().sum::<usize>();
+
+            if total == k_total_half {
+                print!("{}", total);
+                return;
+            }
+
+            if total > k_total_half && total < ans {
+                ans = total;
+            }
+        }
+    }
+
+    print!("{}", ans);
 }
