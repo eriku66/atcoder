2bfc88a8fb80ae49e4649628a91fce8724c57a21
eriku66 <eriku.hashimoto@gmail.com>
2024-10-19 23:45:40 +0900

    solve: 🤩 abc370 c

--- abc370/src/bin/c.rs
+++ abc370/src/bin/c.rs
@@ -7,6 +7,7 @@ use proconio::{
     fastout, input,
     marker::{Chars, Usize1},
 };
+use std::cmp::Ordering;
 #[allow(unused_imports)]
 use std::cmp::{max, min};
 #[allow(unused_imports)]
@@ -16,9 +17,40 @@ use std::iter::FromIterator;
 #[allow(unused_imports)]
 use superslice::Ext;
 
-#[fastout]
 fn main() {
     input! {
-        n: usize,
+        mut s: Chars,
+        t: Chars,
+    }
+
+    let mut x = Vec::new();
+
+    let o_i_list = s
+        .iter()
+        .zip(t.iter())
+        .enumerate()
+        .map(|(i, (c, h))| (i, c.cmp(&h)))
+        .collect_vec();
+
+    for &(i, o) in o_i_list.iter() {
+        if o == Ordering::Greater {
+            s[i] = t[i];
+
+            x.push(s.iter().join(""));
+        }
+    }
+
+    for &(i, o) in o_i_list.iter().rev() {
+        if o == Ordering::Less {
+            s[i] = t[i];
+
+            x.push(s.iter().join(""));
+        }
+    }
+
+    println!("{}", x.len());
+
+    for e in x {
+        println!("{}", e);
     }
 }
