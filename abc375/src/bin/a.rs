142672b2fb1efeb4987b16cb53ea2ce27caa6ed8
eriku66 <eriku.hashimoto@gmail.com>
2024-10-25 23:13:18 +0900

    solve: 🤩 abc375 a

---
+++ abc375/src/bin/a.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input! {
+        _: usize,
+        s: Chars,
+    }
+
+    print!(
+        "{}",
+        s.windows(3)
+            .filter(|w| w[0] == '#' && w[2] == '#' && w[1] == '.')
+            .count()
+    );
+}
---
+++ abc375/src/bin/g.rs
@@ -0,0 +1,23 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+}
