458782706c33c35632078652b7df26a12c250e57
eriku66 <eriku.hashimoto@gmail.com>
2024-11-01 00:04:08 +0900

    solve: 🤩 abc376 c

--- abc376/src/bin/c.rs
+++ abc376/src/bin/c.rs
@@ -19,5 +19,38 @@ use superslice::Ext;
 fn main() {
     input! {
         n: usize,
+        mut a_list: [usize; n],
+        mut b_list: [usize; n - 1],
     }
+
+    a_list.sort_unstable();
+    b_list.sort_unstable();
+
+    let mut x = 0;
+
+    for a in a_list.into_iter().rev() {
+        if b_list.len() == 0 {
+            x = a;
+
+            break;
+        }
+
+        let last_b = *b_list.last().unwrap();
+
+        if a <= last_b {
+            b_list.pop();
+
+            continue;
+        }
+
+        if x != 0 {
+            print!("-1");
+
+            return;
+        }
+
+        x = a;
+    }
+
+    print!("{}", x);
 }
