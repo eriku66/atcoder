ca89b2220077cf82d1b7c8697dd9228fbfb404e3
eriku66 <eriku.hashimoto@gmail.com>
2024-11-13 23:55:24 +0900

    solve: 🤩 abc379 a

---
+++ abc379/src/bin/a.rs
@@ -0,0 +1,25 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use superslice::Ext;
+
+fn main() {
+    input! {
+        n: Chars,
+    }
+
+    print!("{}{}{} {}{}{}", n[1], n[2], n[0], n[2], n[0], n[1]);
+}
