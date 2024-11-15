4e6c1f8f116e3865d3334980cd312bc904bc1b74
eriku66 <eriku.hashimoto@gmail.com>
2024-11-15 23:56:13 +0900

    solve: 🤩 abc322 c

--- abc322/src/bin/c.rs
+++ abc322/src/bin/c.rs
@@ -19,5 +19,26 @@ use superslice::Ext;
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        a_list: [Usize1; m],
+    }
+
+    let set: HashSet<usize> = HashSet::from_iter(a_list.into_iter());
+
+    let mut ans = Vec::new();
+    let mut diff = 0;
+
+    for i in (0..n).rev() {
+        if set.contains(&i) {
+            diff = 0;
+        } else {
+            diff += 1;
+        }
+
+        ans.push(diff);
+    }
+
+    for d in ans.into_iter().rev() {
+        println!("{}", d);
     }
 }
