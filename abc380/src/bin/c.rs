54783005cd967c4f1163c1d9cc3ddd2c20695e68
eriku66 <eriku.hashimoto@gmail.com>
2024-11-20 00:29:24 +0900

    solve: 🤩 abc380 c

--- abc380/src/bin/c.rs
+++ abc380/src/bin/c.rs
@@ -18,6 +18,33 @@ use superslice::Ext;
 
 fn main() {
     input! {
-        n: usize,
+        _n: usize,
+        mut k: usize,
+        s: Chars,
     }
+
+    let mut pre_c = '.';
+    let mut masses: Vec<String> = Vec::new();
+    let mut swap_idx = 0;
+
+    for c in s {
+        if c == pre_c {
+            masses.last_mut().unwrap().push(c);
+        } else {
+            masses.push(c.to_string());
+
+            if k > 0 && c == '1' {
+                k -= 1;
+
+                if k == 0 {
+                    swap_idx = masses.len() - 1;
+                }
+            }
+        }
+
+        pre_c = c;
+    }
+    masses.swap(swap_idx - 1, swap_idx);
+
+    println!("{}", masses.join(""));
 }
